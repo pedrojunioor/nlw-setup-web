373d704b26647e8d0fa079792e6822381eda73ba
pedro junior <pedro.junior@infarma.com.br>
2023-04-23 18:46:28 -0300

    initial commmit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css'
+import { Habit } from "./components/Habit"
+
+function App() {
+
+  return (
+    <>
+      <Habit completed={1} />
+    </>
+  )
+}
+
+export default App
